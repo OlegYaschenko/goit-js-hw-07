e3ed7e372b40ad6258eea6baeca32e91b61513c5
OlegYaschenko <olegyaschenko@gmail.com>
2021-12-17 23:51:34 +0200

    тест1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const instance = basicLightbox.create(
+  `<img src="" width="800" height="600">`,
+   {
+      onShow: (instance) => {
+       document.addEventListener('keydown', onPressEscCloseModal);
+      },
+      onClose: (instance) => {
+        document.removeEventListener('keydown', onPressEscCloseModal);
+      },
+    } 
+);
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  image: instance.element().querySelector('img'),
+}
+
+
+const createMarkupEl = galleryItems.map(({ original, preview, description}) =>
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`)
+.join('');
+
+
+refs.gallery.innerHTML = createMarkupEl;
+
+refs.gallery.addEventListener('click', onOpenModal);
+
+function onOpenModal(e) { 
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") { 
+    return
+  }
+  
+  refs.image.src = e.target.dataset.source;
+  instance.show();
+}
+
+function onPressEscCloseModal(e) { 
+  if (e.code === 'Escape') {
+    instance.close();
+   }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+}
+
+const createMarkupEl = galleryItems.map(({ original, preview, description}) =>
+  `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`)
+.join('');
+
+refs.gallery.innerHTML = createMarkupEl;
+
+new SimpleLightbox('.gallery a', { 
+  captionDelay: 250,
+  captionsData: 'alt',
+});
